521ae9e5a04155bf950e1770943b9c0de1cf3615
Jahid-HasanM <web.businessp@gmail.com>
2025-11-04 18:21:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+
+app.listen(port ,(err)=>{
+    if(err) console.log('err');
+    console.log('server is running on port 4000');
+})
+
+app.get('/userOne' , (req , res)=>{
+    res.send('updet system')
+})
+app.post('/login' , (req , res)=>{
+    res.send('hightmthetode')
+})
+
